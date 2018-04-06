9406bc3bc2b0e7ae07972a8676da13c92e2116d7
dmry <dmry.nl>
2018-04-06 14:41:48 +0200

    Removed unnecessary overload for Solve().

--- Namics/newton.h
+++ Namics/newton.h
@@ -15,7 +15,7 @@ public:
 
 ~Newton();
 
-	string name; 
+	string name;
 //*************scheutjens **************
 
 	int lineiterations,linetolerance,resetiteration;
@@ -25,46 +25,46 @@ public:
 	Real smallAlpha;
 	int maxNumSmallAlpha;
 	int numIterationsSinceHessian;
-	int smallAlphaCount; 
+	int smallAlphaCount;
 	int reverseDirectionRange;
 	int numReverseDirection;
 	Real maxFrReverseDirection;
 	Real minAccuracyForHessian;
-	Real minAccuracySoFar; 
+	Real minAccuracySoFar;
 	Real resetHessianCriterion;
 	Real trustregion;
 	Real trustfactor;
 	bool newtondirection;
 	bool reset_pseudohessian;
 	bool pseudohessian;
-	bool hessian; 
+	bool hessian;
 	bool samehessian;
 	Real accuracy;
 	Real max_accuracy_for_hessian_scaling;
-	int n_iterations_for_hessian; 
+	int n_iterations_for_hessian;
 	Real ALPHA;
 	bool ignore_newton_direction;
 	Real delta_min;
 	int nbits;
 	int trouble;
-	int hessianwidth;  
+	int hessianwidth;
 	Real alphaMax,alphaMin,alphabound,minimum,normg;
 //***************************
-	int *reverseDirection; 
+	int *reverseDirection;
 	Real delta_max;
 	bool e_info;
 	bool s_info;
 	bool super_e_info;
 	bool super_s_info;
-	vector<Input*> In; 
+	vector<Input*> In;
 	vector<System*> Sys;
 	vector<Segment*> Seg;
-	vector<Lattice*> Lat;  
+	vector<Lattice*> Lat;
 	vector<Molecule*> Mol;
 	vector<Variate*> Var;
 	int iterationlimit,m,i_info;
 	int super_iterationlimit;
-	int k_diis,it; 
+	int k_diis,it;
 	int n_tr,n_reset,n_ignore;
 	int start;
 	Real tolerance;
@@ -73,9 +73,9 @@ public:
 	Real epsilon;
 	string method;
 	string StoreFileGuess;
-	string ReadFileGuess;  
+	string ReadFileGuess;
 	string stop_criterion;
-	int iv; 
+	int iv;
 
 	vector<string> ints;
 	vector<string> Reals;
@@ -92,7 +92,7 @@ public:
 	void PushOutput();
 	int GetValue(string,int&,Real&,string&);
 
-	
+
 	Real* xx;
 	Real* x0;
 	Real* g;
@@ -101,7 +101,7 @@ public:
 	Real* alpha;
 	Real* Aij;
 	Real* Ci;
-	Real* Apij; 
+	Real* Apij;
 	Real* p;
 	Real* p0;
 	Real* g0;
@@ -109,23 +109,22 @@ public:
 	int* mask;
 	Real* r;
 	Real* d;
-	Real* q; 
-	Real* s; 
-		
+	Real* q;
+	Real* s;
+
 	std::vector<string> KEYS;
 	std::vector<string> PARAMETERS;
 	std::vector<string> VALUES;
 	bool CheckInput(int);
-	void PutParameter(string); 
-	string GetValue(string); 
+	void PutParameter(string);
+	string GetValue(string);
 	void Copy(Real*,Real*,int,int,int);
 	bool Guess(Real*,string,vector<string>,bool,int,int,int);
 	string GetNewtonInfo(int&);
 	bool Solve(bool);
-	bool Solve(int,int,int,int);
 	bool SuperIterate(int,int,int,int);
 	void DeAllocateMemory();
-	void AllocateMemory(); 
+	void AllocateMemory();
 	bool PrepareForCalculations(void);
 	void Ax(Real* , Real* , int );
 	void DIIS(Real* , Real* , Real* , Real*, Real* ,Real* , int ,int, int , int );
@@ -140,27 +139,27 @@ public:
 	bool SolvePsi(Real*, Real*, Real*);
 	bool Iterate_Picard();
 	bool Iterate_DIIS();
-	void Message(bool,bool,int, int,Real, Real,string); 
+	void Message(bool,bool,int, int,Real, Real,string);
 	bool PutU();
-	
+
 //**********Scheutjens****************
-	
+
 	void iterate(Real*,int); //there is only one iterate;
 	void newdirection(float*, Real*, Real*, Real*, Real*, Real*, int, Real); //there is only one of this.
-	void inneriteration(float*, Real*,Real*,Real, int);  
+	void inneriteration(float*, Real*,Real*,Real, int);
 	void direction(float*, Real*, Real*, Real*, Real*, int, Real);
 	void newhessian(float*,Real*,Real*,Real*,Real*,int);
 	void resethessian(float*, Real*, Real*, int);
 	void startderivatives(float*,Real*,Real*,int);
 
-	void newtrustregion(Real*,Real*,Real*,Real*,int); //there is only one. 
-	Real linesearch(Real*,Real*,Real*,Real*,Real*,int, Real);  //there is only one. 
+	void newtrustregion(Real*,Real*,Real*,Real*,int); //there is only one.
+	Real linesearch(Real*,Real*,Real*,Real*,Real*,int, Real);  //there is only one.
 	Real zero(Real*,Real*,Real*,Real*,Real*,int,Real);
 	Real stepchange(Real*,Real*,Real*,Real*,Real*,Real*,int,Real&);
-	Real linecriterion(Real*, Real*, Real*, Real*,int); 
+	Real linecriterion(Real*, Real*, Real*, Real*,int);
 	void numhessian(float*, Real*, Real*, int);
 	void findhessian(float* ,Real*,Real*,int);
-	void decomposition(float*,int,int&); 
+	void decomposition(float*,int,int&);
 	Real norm2(Real*,int);
 	void decompos(float*, int, int&);
 	int signdeterminant(float*, int);
@@ -168,9 +167,9 @@ public:
 	void updateneg(float* ,Real* , int, Real);
 	void updatpos(float*, Real*, Real*, int, Real);
 	void gausa(float*, Real*, Real*, int);
-	void gausb(float*, Real*, int);	
-	Real newfunction(Real*, Real*, int);	
-	Real residue(Real*, Real*, Real*, int, Real);	
+	void gausb(float*, Real*, int);
+	Real newfunction(Real*, Real*, int);
+	Real residue(Real*, Real*, Real*, int, Real);
 //************************************
 };
 #endif

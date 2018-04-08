d83c8f8c8d6c7a3d92d8fe4c38fb65ab01fb299c
Dmry <daniel@dmry.nl>
2018-04-08 16:15:48 +0200

    Commented code, improved legibility.

--- Namics/namics.cpp
+++ Namics/namics.cpp
@@ -66,17 +66,17 @@ int main(int argc, char *argv[]) {
 #endif
 
 //All class instances are stored in the following vectors
-	vector<Input*> In;			//Inputs read from file
-	vector<Output*> Out;		//Outputs written to file
+	vector<Input*> In;	//Inputs read from file
+	vector<Output*> Out;	//Outputs written to file
 	vector<Lattice*> Lat; 	//Properties of the lattice
 	vector<Molecule*> Mol;	//Properties of entire molecule
 	vector<Segment*> Seg; 	//Properties of molecule segments
-	vector<Newton*> New;		//Solvers and iteration schemes
+	vector<Newton*> New;	//Solvers and iteration schemes
 	vector<System*> Sys;
 	vector<Engine*> Eng;
 	vector<Variate*> Var;
 
-//Create input class instance and handle errors(reference above)
+	//Create input class instance and handle errors(reference above)
 	In.push_back(new Input(filename));
 	if (In[0]->Input_error) {return 0;}
  	n_starts=In[0]->GetNumStarts();
@@ -91,13 +91,13 @@ int main(int argc, char *argv[]) {
 	In[0]->MakeLists(start);
 	cout <<"Problem nr " << start << " out of " << n_starts << endl;
 
-/******** Class creation starts here ********/
+		/******** Class creation starts here ********/
 
-  //Create lattice class instance and check inputs (reference above)
+		//Create lattice class instance and check inputs (reference above)
 		Lat.push_back(new Lattice(In,In[0]->LatList[0]));
 		if (!Lat[0]->CheckInput(start)) {return 0;}
 
-  //Create segment class instance and check inputs (reference above)
+		//Create segment class instance and check inputs (reference above)
 		int n_seg=In[0]->MonList.size();
 		for (int i=0; i<n_seg; i++) Seg.push_back(new Segment(In,Lat,In[0]->MonList[i],i,n_seg));
 		for (int i=0; i<n_seg; i++) {
@@ -107,20 +107,20 @@ int main(int argc, char *argv[]) {
 			if (!Seg[i]->CheckInput(start)) return 0;
 		}
 
-	//Create segment class instance and check inputs (reference above)
+		//Create segment class instance and check inputs (reference above)
 		int n_mol = In[0]->MolList.size();
 		for (int i=0; i<n_mol; i++) {
 			Mol.push_back(new Molecule(In,Lat,Seg,In[0]->MolList[i]));
 			if (!Mol[i]->CheckInput(start)) return 0;
 		}
 
-  //Create system class instance and check inputs (reference above)
+		//Create system class instance and check inputs (reference above)
 		Sys.push_back(new System(In,Lat,Seg,Mol,In[0]->SysList[0])); Sys[0]->cuda=cuda;
 		if (!Sys[0]->CheckInput(start)) {return 0;}
 		if (!Sys[0]->CheckChi_values(n_seg)) return 0;
 
-	//Prepare variables used in variate class creation
-	//TODO: What do these variables mean?
+		//Prepare variables used in variate class creation
+		//TODO: What do these variables do?
 		int n_var=In[0]->VarList.size();
 		int n_search=0;
 		int n_scan=0;
@@ -129,7 +129,7 @@ int main(int argc, char *argv[]) {
 		int n_target=0;
 		int search_nr=-1,scan_nr=-1,target_nr=-1,ets_nr=-1,etm_nr=-1;
 
-  //Create variate class instance and check inputs (reference above)
+		//Create variate class instance and check inputs (reference above)
 		for (int k=0; k<n_var; k++) {
 			Var.push_back(new Variate(In,Lat,Seg,Mol,Sys,In[0]->VarList[k]));
 			if (!Var[k]->CheckInput(start)) {return 0;}
@@ -141,7 +141,7 @@ int main(int argc, char *argv[]) {
 			if (Var[k]->eq_to_mu>-1) {etm_nr=k; n_etm++;}
 		}
 
-	//Error code for faulty variate class creation
+		//Error code for faulty variate class creation
 		if (n_etm>1) {cout <<"too many equate_to_mu's in var statements. The limit is 1 " << endl; return 0;}
 		if (n_ets>1) {cout <<"too many equate_to_solvent's in var statements. The liZZmit is 1 " << endl; return 0;}
 		if (n_search>1) {cout << "too many 'search'es in var statements. The limit is 1 " << endl; return 0;}
@@ -150,28 +150,27 @@ int main(int argc, char *argv[]) {
 		if (n_search>0 && n_target==0) {cout <<"lonely search. Please specify in 'var' a target function, e.g. 'var : sys-NN : grand_potential : 0'" << endl;  return 0 ;}
 		if (n_target>0 && n_search==0) {cout <<"lonely target. Please specify in 'var' a search function, e.g. 'var : mol-lipid : search : theta' " << endl; return 0;}
 
-  //Create newton class instance and check inputs (reference above)
+		//Create newton class instance and check inputs (reference above)
 		New.push_back(new Newton(In,Lat,Seg,Mol,Sys,Var,In[0]->NewtonList[0]));
 		if (!New[0]->CheckInput(start)) {return 0;}
 
-  //Create engine class instance and check inputs (reference above)
+		//Create engine class instance and check inputs (reference above)
 		Eng.push_back(new Engine(In,Lat,Seg,Mol,Sys,New,In[0]->EngineList[0]));
 		if (!Eng[0]->CheckInput(start)) {return 0;}
 
-  //Prepare, catch errors for output class creation
+		//Prepare, catch errors for output class creation
 		int n_out = In[0]->OutputList.size();
 		if (n_out ==0) cout <<"Warning: no output defined" << endl;
 
-	//Create output class instance and check inputs (reference above)
+		//Create output class instance and check inputs (reference above)
 		for (int i=0; i<n_out; i++) {
 			Out.push_back(new Output(In,Lat,Seg,Mol,Sys,New,Eng,In[0]->OutputList[i],i,n_out));
 			if (!Out[i]->CheckInput(start)) {cout << "input_error in output " << endl; return 0;}
 		}
 
-		//TODO: 1. 	I think sys.initial_guess is always set to "previous_result" by default at system.cpp:301
-		//					which would make the following obsolete.
-		//			2.	What does this block do, exactly? It seems to handle some code for charged molecules.
-		//			3.  Can we move this to the system class bit?
+		//TODO: 1. sys.initial_guess is set to "previous_result" by default at system.cpp:301
+		//	2. What does this block do, exactly? It seems to handle some code for charged molecules.
+		//	3. Can we move this to the system class bit?
 
 		if (Sys[0]->initial_guess=="file") {
 			MONLIST.clear();
@@ -214,7 +213,7 @@ int main(int argc, char *argv[]) {
 				New[0]->SuperIterate(search_nr,target_nr,ets_nr,etm_nr);
 			}
 
-/********** Generate output from every class *********/
+/********** Output information about all classes to file *********/
 			Lat[0]->PushOutput();
 			New[0]->PushOutput();
 			Eng[0]->PushOutput();
@@ -237,7 +236,7 @@ int main(int argc, char *argv[]) {
 		}
 
 /******** Clear all class instances ********/
-		//TODO: Again some code for	charged molecules?
+		//TODO: Again some code for ..charged molecules?
 		if (scan_nr>-1) Var[scan_nr]->ResetScanValue();
 		if (Sys[0]->initial_guess=="previous_result") {
 			int IV;

ca5a53b854202a2ddbf0f172753f9ae23c9357ca
Dmry <daniel@dmry.nl>
2018-11-28 01:04:21 +0100

    Abstracted host & device tools to allow CUDA and Newton to function as intended.

--- src/tools.h
+++ src/tools.h
@@ -76,6 +76,8 @@ void Sum(int&, int*, int);
 bool GPU_present();
 int* AllIntOnDev(int);
 Real* AllOnDev(int);
+int* AllManagedIntOnDev(int);
+Real* AllManagedOnDev(int);
 void AddTimes(Real*, Real*, Real*, int);
 void Times(Real*, Real*, Real*, int);
 void Times(Real*, Real*, int*, int);
@@ -122,97 +124,14 @@ void UpPsi(Real*, Real*, Real*, Real*, int, int, Real, int*, int);
 
 #else
 
-template <typename T>
-inline void Times(Real* P, Real* A, T* B, int M) {
-  std::transform(A, A + M, B, P, std::multiplies<Real>());
-}
+#include "tools_host.h"
+
+#endif
 
 template <typename T>
-inline void Add(T* P, T* A, int M) {
-  std::transform(P, P + M, A, P, std::plus<T>());
-}
-
-template <typename T>
-inline void Sum(T &result, T *x,int M)   {
-  result = 0;
-  for (int i=0; i<M; i++) result +=x[i];
-}
-
-template <typename T>
-inline void Invert(T* KSAM, T* MASK, int M) {
-  std::transform(MASK, MASK + M, KSAM, KSAM, [](Real A, Real B) { if (A==0) return 1.0; else return 0.0; });
-}
-
-template <typename T>
-inline void Zero(T* P, int M) {
-  std::fill(P, P + M, 0);
-}
-
-template <typename T>
-inline void Cp(Real* P, T* A, int M) {
+inline void H_Cp(Real* P, T* A, int M) {
   std::copy(A, A + M, P);
 }
-
-void bx(Real*, int, int, int, int, int, int, int);
-void b_x(Real*, int, int, int, int, int, int, int);
-void by(Real*, int, int, int, int, int, int, int);
-void b_y(Real*, int, int, int, int, int, int, int);
-void bz(Real*, int, int, int, int, int, int, int);
-void b_z(Real*, int, int, int, int, int, int, int);
-void bx(int*, int, int, int, int, int, int, int);
-void b_x(int*, int, int, int, int, int, int, int);
-void by(int*, int, int, int, int, int, int, int);
-void b_y(int*, int, int, int, int, int, int, int);
-void bz(int*, int, int, int, int, int, int, int);
-void b_z(int*, int, int, int, int, int, int, int);
-void Dot(Real&, Real*, Real*, int);
-bool GPU_present();
-Real* AllOnDev(int);
-int* AllIntOnDev(int);
-void AddTimes(Real*, Real*, Real*, int);
-void Composition(Real*, Real*, Real*, Real*, Real, int);
-void Norm(Real*, Real, int);
-void Unity(Real*, int);
-void YisAplusCtimesB(Real*, Real*, Real*, Real, int);
-void YisAminB(Real*, Real*, Real*, int);
-void YisAplusC(Real*, Real*, Real, int);
-void YisAplusB(Real*, Real*, Real*, int);
-void YplusisCtimesX(Real*, Real*, Real, int);
-void UpdateAlpha(Real*, Real*, Real, int);
-void Picard(Real*, Real*, Real, int);
-void Dubble(Real*, Real*, Real, int);
-void MinLog(Real*, Real*, int);
-void Boltzmann(Real*, Real*, int);
-void AddGradSquare(Real*, Real*, Real*, Real*, int);
-void PutAlpha(Real*, Real*, Real*, Real, Real, int);
-void PutAlpha(Real*, Real*, Real, Real, int);
-void Div(Real*, Real*, int);
-void AddG(Real*, Real*, Real*, int);
-void OneMinusPhitot(Real*, Real*, int);
-void ComputeGN(Real*, Real*, int, int);
-namespace tools {
-void DistributeG1(Real*, Real*, int*, int*, int*, int, int, int, int, int, int, int, int, int, int, int, int, int);
-void CollectPhi(Real*, Real*, Real*, int*, int*, int*, int, int, int, int, int, int, int, int, int, int, int, int, int);
-}
-void OverwriteC(Real*, int*, Real, int);
-void OverwriteA(Real*, int*, Real*, int);
-void UpQ(Real*, Real*, Real*, Real*, int, int, Real, int*, int);
-void UpPsi(Real*, Real*, Real*, Real*, int, int, Real, int*, int);
-
-template <typename T>
-inline void SetBoundaries(T* P, int jx, int jy, int bx1, int bxm, int by1, int bym, int bz1, int bzm, int Mx, int My, int Mz) {
-  bx(P, Mx + 1, My + 2, Mz + 2, bx1, bxm, jx, jy);
-  by(P, Mx + 2, My + 1, Mz + 2, by1, bym, jx, jy);
-  bz(P, Mx + 2, My + 2, Mz + 1, bz1, bzm, jx, jy);
-}
-
-template <typename T>
-inline void RemoveBoundaries(T* P, int jx, int jy, int bx1, int bxm, int by1, int bym, int bz1, int bzm, int Mx, int My, int Mz) {
-  b_x(P, Mx + 1, My + 2, Mz + 2, bx1, bxm, jx, jy);
-  b_y(P, Mx + 2, My + 1, Mz + 2, by1, bym, jx, jy);
-  b_z(P, Mx + 2, My + 2, Mz + 1, bz1, bzm, jx, jy);
-}
-#endif
 
 template <typename T>
 inline void H_Zero(T* H_P, int M) {
@@ -239,7 +158,9 @@ inline void H_Invert(T* KSAM, T* MASK, int M) {
   transform(MASK, MASK + M, KSAM, KSAM, [](Real A, Real B) {if (A==0) return 1.0; else return 0.0;});
 }
 
-Real pythag(Real, Real);
-int svdcmp(Real**, int, int, Real*, Real**);
+template<typename T>
+void H_PutAlpha(T *g, T *phitot, T *phi_side, T chi, T phibulk, int M)   {
+	for (int i=0; i<M; i++) if (phitot[i]>0) g[i] = g[i] - chi*(phi_side[i]/phitot[i]-phibulk);
+}
 
 #endif

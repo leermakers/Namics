cce52d8c4876759a4a2bc01ec987b67c623a8ed9
A.D. Kazakov <alexander.d.kazakov@gmail.com>
2018-12-06 10:59:07 +0100

    continue working on checkpoints

--- src/cleng/checkpoint/checkpoint.cpp
+++ src/cleng/checkpoint/checkpoint.cpp
@@ -1,20 +1,74 @@
+//
+//#include "../../input.h"
+//#include "../../namics.h"
+//#include "../../solve_scf.h"
+//#include "../../system.h"
+//#include "../../output.h"
+//
+//#include "../cleng.h"
+
+#include <fstream>
+
 #include "checkpoint.h"
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <vector>
 
-const std::string Checkpoint::IN_CLASS_NAME = "checkpoint";
-const std::string Checkpoint::DEFAULT_OUTPUT_PATH = "./checkpoints/";
+using namespace std;
 
-//Checkpoint::Checkpoint() {
-//    checkpoint_path = DEFAULT_OUTPUT_PATH;
-//}
-//
-//void Checkpoint::addProperty(const std::string &elem, const std::string &param, const std::string &value) {
-//    if (elem == "folder") {
-//        addFolderInfo(param,value);
+const string Checkpoint::IN_CLASS_NAME = "checkpoint";
+const string Checkpoint::DEFAULT_CHECKPOINT_PATH = "./checkpoints/";
+
+Checkpoint::Checkpoint() {
+    checkpoint_path = DEFAULT_CHECKPOINT_PATH;
+}
+
+void Checkpoint::addFolderInfo(const string &param, const string &value) {
+    if (param=="path") {
+        checkpoint_path = processCheckpointPath(value);
+    }
+}
+
+
+void Checkpoint::addProperty(const string &elem, const string &param, const string &value) {
+    if (elem == "folder") {
+        addFolderInfo(param,value);
+    }
+}
+
+const string &Checkpoint::getCheckpointPath() const {
+    return checkpoint_path;
+}
+
+string Checkpoint::processCheckpointPath(string path) {
+    if (path.back() != '/') {
+        path.push_back('/');
+    }
+    return path;
+}
+
+bool Checkpoint::isCheckpointExists() const {
+    struct stat info{};
+    return (stat(checkpoint_path.c_str(), &info) == 0) && (info.st_mode & S_IFDIR) != 0;
+}
+
+void Checkpoint::saveCheckpoint() {
+
+    string filename;
+    vector<string> sub;
+    string infilename = "test";
+//    In[0]->split(infilename,'.',sub);
+//    filename=sub[0].append(".");
+//    filename = sub[0].append("_").append(".").append(name);
+//    filename = In[0]->output_info.getOutputPath() + filename;
+    ofstream outfile;
+    outfile.open(filename+"."+Checkpoint::IN_CLASS_NAME, std::ios_base::app);
+
+// Writing
+//    outfile << MS_step << " ";
+//    for ( auto n : distPerMC ) {
+//        outfile << n << " ";
 //    }
-//}
-//
-//void Checkpoint::addFolderInfo(const std::string &param, const std::string &value) {
-//    if (param=="path") {
-//        checkpoint_path = processCheckpointPath(value);
-//    }
-//}
+    outfile << "Hello Checkpoint" << endl;
+}
--- src/cleng/checkpoint/checkpoint.h
+++ src/cleng/checkpoint/checkpoint.h
@@ -1,31 +1,28 @@
-#ifndef NAMICS_CHECKPOINT_H
-#define NAMICS_CHECKPOINT_H
+#pragma once
+#include <string>
 
-#include "../../output_info.h"
+using namespace std;
 
 class Checkpoint {
-
 public:
     Checkpoint();
 
     void saveCheckpoint();
     void loadCheckpoint();
 
-    void addProperty(const std::string &elem, const std::string &param, const std::string &value);
-    void addFolderInfo(const std::string &param, const std::string &value);
+    void addProperty(const string &elem, const string &param, const string &value);
+    void addFolderInfo(const string &param, const string &value);
 
-    const std::string &getCheckpointPath() const;
+    const string &getCheckpointPath() const;
     bool isCheckpointExists() const;
 
-    static const std::string IN_CLASS_NAME;
-    static const std::string DEFAULT_OUTPUT_PATH;
+    static const string IN_CLASS_NAME;
+    static const string DEFAULT_CHECKPOINT_PATH;
 
 private:
 
-    std::string checkpoint_path;
-    std::string processCheckpointPath(std::string path);
+    string checkpoint_path;
+    string processCheckpointPath(std::string path);
 
 };
 
-
-#endif //NAMICS_CHECKPOINT_H

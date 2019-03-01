09c4d61c4bc41d4c749d88664bf3bd38a7d0dc1e
Daniel Emmery <daniel@dmry.nl>
2019-03-01 22:10:53 +0100

    Better support for different lattice_object norming methods.

---
+++ src/mesodyn/collection_procedures.h
@@ -0,0 +1,50 @@
+#include <map>
+#include "density_initializer.h"
+#include "../molecule.h"
+#include "../tools.h"
+#include "lattice_object.h"
+#include "component.h"
+#include <assert.h>
+
+//Virtual base class  
+class Lattice_object_collection_procedure {
+  public:
+    Lattice_object_collection_procedure() { }
+    ~Lattice_object_collection_procedure() { }
+    Lattice_object_collection_procedure(const Lattice_object_collection_procedure&) { }
+
+    virtual void execute() = 0;
+};
+
+class Order_parameter : public Lattice_object_collection_procedure {
+  public:
+
+    Order_parameter(vector< shared_ptr<IComponent> > components_, std::map<size_t, size_t> combinations_, Real boundaryless_volume_);
+    ~Order_parameter() { }
+
+    void execute() override;
+    Real get();
+
+  private:
+    Real m_boundaryless_volume;
+    Real m_order_parameter;
+    std::vector< shared_ptr<IComponent> > m_components;
+    std::map<size_t, size_t> m_combinations;
+};
+
+class Norm_densities : public Lattice_object_collection_procedure {
+  public:
+
+    Norm_densities(vector<Molecule*> mol_, vector< shared_ptr<IComponent> > components_, size_t solvent_mol);
+
+    void execute() override;
+
+  private:
+
+    std::map<shared_ptr< IComponent >, Real> theta;
+    std::vector< shared_ptr<IComponent> > m_components;
+    size_t m_system_size;
+    size_t m_solvent;
+
+    void fetch_theta(vector<Molecule*> mol, vector< shared_ptr<IComponent> > component);
+};

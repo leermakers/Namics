48d1366ee78a69c8429bb88a9cd1f036be8acdb3
Daniël Emmery <daniel@dmry.nl>
2019-03-08 20:22:47 +0100

    Fixes when running DCUDA but not DPAR_MESODYN.

--- src/mesodyn/collection_procedures.cpp
+++ src/mesodyn/collection_procedures.cpp
@@ -1,64 +1,80 @@
 #include "collection_procedures.h"
 
-Norm_densities::Norm_densities(vector<Molecule*> mol_, vector< shared_ptr<IComponent> > components_, size_t solvent_mol)
-: m_components{components_}, m_system_size{components_[0]->rho.size()}, m_solvent{solvent_mol}
+constexpr uint8_t TOTAL_DENSITY = 1;
+
+Norm_densities::Norm_densities(vector<Molecule *> mol_, vector<shared_ptr<IComponent>> components_, size_t solvent_mol)
+    : m_components{components_}, m_system_size{components_[0]->rho.size()}, m_solvent{solvent_mol}
 {
-    assert(mol_[ solvent_mol ]->MolMonList.size() == 1 and "Norming solvents with mutliple monomers is not supported! Please write your own script");
-    fetch_theta(mol_, components_);
+  assert(mol_[solvent_mol]->MolMonList.size() == 1 and "Norming solvents with mutliple monomers is not supported! Please write your own script");
+  fetch_theta(mol_, components_);
 }
 
-void Norm_densities::execute() {
-    //TODO: when norming after theta in mol changed: fetch_theta before running this.
-    stl::device_vector<Real> residuals(m_system_size ,0);
-    for (auto component : m_components) {
-      Norm( (Real*)component->rho, ( theta[component]/component->theta() ), m_system_size);
-      stl::transform(component->rho.begin(), component->rho.end(), residuals.begin(), residuals.begin(), stl::plus<Real>());
+void Norm_densities::execute()
+{
+  //TODO: when norming after theta in mol changed: fetch_theta before running this.
+  stl::device_vector<Real> residuals(m_system_size, 0);
+  for (auto component : m_components)
+  {
+#ifdef PAR_MESODYN
+    Norm((Real *)component->rho, (theta[component] / component->theta()), m_system_size);
+#else
+    Real comp_theta = component->theta();
+    for (Real &value : component->rho)
+      value *= (theta[component] / comp_theta);
+#endif
+    stl::transform(component->rho.begin(), component->rho.end(), residuals.begin(), residuals.begin(), stl::plus<Real>());
+  }
+  stl::transform(residuals.begin(), residuals.end(), m_components[m_solvent]->rho.begin(), m_components[m_solvent]->rho.begin(),
+                 [this] DEVICE_LAMBDA(const double &x, const double &y) { return (y - (x - TOTAL_DENSITY)); });
+}
+
+void Norm_densities::fetch_theta(vector<Molecule *> mol, vector<shared_ptr<IComponent>> component)
+{
+  int j = 0;
+  for (auto &molecule : mol)
+  {
+    Molecule_density this_density = Molecule_density(molecule);
+
+    vector<Real> densities = this_density.monomer_total_mass();
+    for (auto density : densities)
+    {
+
+      theta[component[j]] = density;
+
+      if (density == 0)
+        m_solvent = j;
+
+      ++j;
     }
-    stl::transform(residuals.begin(), residuals.end(), m_components[m_solvent]->rho.begin(), m_components[m_solvent]->rho.begin(),
-      [this] DEVICE_LAMBDA (const double& x, const double& y) { return ( y-(x-1));} );
+  }
 }
 
-void Norm_densities::fetch_theta(vector<Molecule*> mol, vector< shared_ptr<IComponent> > component) {
-    int j = 0;
-    for (auto& molecule : mol) {
-        Molecule_density this_density = Molecule_density(molecule);
-
-        vector<Real> densities = this_density.monomer_total_mass();
-        for (auto density : densities) {
-      
-            theta[component[j]] = density;
-
-            if (density == 0)
-                m_solvent = j;
-
-            ++j;
-        }
-    }
+Order_parameter::Order_parameter(vector<shared_ptr<IComponent>> components_, std::map<size_t, size_t> combinations_, Real boundaryless_volume_)
+    : m_boundaryless_volume{boundaryless_volume_}, m_components{components_}, m_combinations{combinations_}
+{
 }
 
-Order_parameter::Order_parameter(vector< shared_ptr<IComponent> > components_, std::map<size_t, size_t> combinations_, Real boundaryless_volume_)
-: m_boundaryless_volume{boundaryless_volume_}, m_components{components_}, m_combinations{combinations_}
-{ }
-
-void Order_parameter::execute() {
+void Order_parameter::execute()
+{
   stl::device_vector<Real> difference(m_components[0]->rho.size());
   m_order_parameter = 0;
-  for (auto& index_of : m_combinations) {
+  for (auto &index_of : m_combinations)
+  {
 
     stl::transform(m_components[index_of.first]->rho.begin(), m_components[index_of.first]->rho.end(), m_components[index_of.second]->rho.begin(),
-      difference.begin(),
-      [this] DEVICE_LAMBDA (const Real& a, const Real& b) mutable {return pow(a-b,2);}
-      );
+                   difference.begin(),
+                   [this] DEVICE_LAMBDA(const Real &a, const Real &b) mutable { return pow(a - b, 2); });
 
-    #ifdef PAR_MESODYN
+#ifdef PAR_MESODYN
     m_order_parameter = thrust::reduce(difference.begin(), difference.end(), m_order_parameter);
-    #else
+#else
     m_order_parameter = std::accumulate(difference.begin(), difference.end(), m_order_parameter);
-    #endif
+#endif
   }
   m_order_parameter /= m_boundaryless_volume;
 }
 
-Real Order_parameter::get() {
-    return m_order_parameter;
+Real Order_parameter::get()
+{
+  return m_order_parameter;
 }

92f6b66ca0c8887bb5559342557c8dcd0c84b6c3
Daniël Emmery <daniel@dmry.nl>
2019-05-23 15:24:40 +0200

    Cleaned up branch

--- rename_cpp_to_cu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find src/mesodyn -depth -name "*.cpp" -exec sh -c 'mv "$1" "${1%.cpp}.cu"' _ {} \;
-find src -name "tools.cpp" -exec sh -c 'mv "$1" "${1%.cpp}.cu"' _ {} \;
-find src/ -name "mesodyn.cpp" -exec sh -c 'mv "$1" "${1%.cpp}.cu"' _ {} \;
--- rename_cu_to_cpp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find src/ -depth -name "*.cu" -exec sh -c 'mv "$1" "${1%.cu}.cpp"' _ {} \;

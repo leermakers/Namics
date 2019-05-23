85117dfe65c02403ba92b5bf53e86eb165105ead
ramanathan <ramanathan.varadharajan@outlook.com>
2019-05-23 11:41:17 +0200

    .gitignore fixed. Please Do not add obj, inputs, bin folders into your commits.

--- obj/cleng/nodes/monolit.d
+++
@@ -1,9 +0,0 @@
-obj/cleng/nodes/monolit.o: src/cleng/nodes/monolit.cpp src/cleng/nodes/monolit.h \
- src/cleng/nodes/node.h src/cleng/nodes/point.h \
- src/cleng/nodes/../../namics.h src/cleng/nodes/simple_node.h
-src/cleng/nodes/monolit.cpp:
-src/cleng/nodes/monolit.h:
-src/cleng/nodes/node.h:
-src/cleng/nodes/point.h:
-src/cleng/nodes/../../namics.h:
-src/cleng/nodes/simple_node.h:

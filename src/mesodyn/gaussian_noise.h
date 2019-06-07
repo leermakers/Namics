08c47f51f3c1b9b19d7877927d63995f19157891
Daniël Emmery <daniel@dmry.nl>
2019-06-07 15:27:55 +0200

    Improved const-correctness for gaussian_noise

--- src/mesodyn/gaussian_noise.h
+++ src/mesodyn/gaussian_noise.h
@@ -17,12 +17,12 @@ public:
   Gaussian_noise(shared_ptr<Boundary1D>, Real, Real); // Not seeded (32 bits of randomness)
   Gaussian_noise(shared_ptr<Boundary1D>, Real, Real, size_t); // Seeded
   int generate(size_t);
-  int add_noise(stl::device_vector<Real>&);
-  int add_noise(Lattice_object<Real>&);
+  int add_noise(stl::device_vector<Real>&) const;
+  int add_noise(Lattice_object<Real>&) const;
   stl::device_vector<Real> noise;
 
 private:
-  seed_seq seed;
+  const seed_seq seed;
   stl::minstd_rand prng;
   stl::normal_distribution<Real> dist;
   shared_ptr<Boundary1D> boundary;

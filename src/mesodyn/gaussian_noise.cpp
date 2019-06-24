1f9af40b6bd78bdca37f868ef8561d18946dd511
Daniël Emmery <daniel@dmry.nl>
2019-06-24 16:59:43 +0200

    New cuRand based noise generation

--- src/mesodyn/gaussian_noise.cpp
+++ src/mesodyn/gaussian_noise.cpp
@@ -1,11 +1,42 @@
 #include "gaussian_noise.h"
 
+
+#ifdef PAR_MESOYN
+Gaussian_noise::Gaussian_noise(shared_ptr<Boundary1D> boundary, Real mean, Real stddev)
+: noise(0), prng{std::random_device{}()}, dist(mean, stddev), boundary{boundary}
+{
+  curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_MT19937);
+  curandSetPseudoRandomGeneratorSeed(gen, rand());
+}
+
+Gaussian_noise::Gaussian_noise(shared_ptr<Boundary1D> boundary, Real mean_, Real stddev_, size_t seed)
+: noise(0), prng(seed), dist(mean_, stddev_), boundary{boundary}
+{
+  mean = mean_; stddev=stddev_;
+  curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_MT19937);
+  curandSetPseudoRandomGeneratorSeed(gen, seed);
+}
+
+Gaussian_noise::~Gaussian_noise() {
+  curandDestroyGenerator(gen);
+}
+
+int Gaussian_noise::generate(size_t system_size) {
+  curandGenerateNormalDouble(gen, thrust::raw_pointer_cast(noise.data()), system_size, mean, stddev);
+
+  boundary->update_boundaries(noise);
+
+  return 0;
+}
+#else
 Gaussian_noise::Gaussian_noise(shared_ptr<Boundary1D> boundary, Real mean, Real stddev)
 : noise(0), prng{std::random_device{}()}, dist(mean, stddev), boundary{boundary}{}
 
 Gaussian_noise::Gaussian_noise(shared_ptr<Boundary1D> boundary, Real mean, Real stddev, size_t seed)
 : noise(0), prng(seed), dist(mean, stddev), boundary{boundary}
 {}
+
+Gaussian_noise::~Gaussian_noise() {}
 
 int Gaussian_noise::generate(size_t system_size) {
   stl::host_vector<Real> tmp_noise(system_size);
@@ -18,6 +49,7 @@ int Gaussian_noise::generate(size_t system_size) {
 
   return 0;
 }
+#endif
 
 int Gaussian_noise::add_noise(stl::device_vector<Real>& target) const{
   assert(noise.size() == target.size());
